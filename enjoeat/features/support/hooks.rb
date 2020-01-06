466637279db96574e11c6342dfcdc347a3fa3227
Cleiton Xavier <cxdossantos@gmail.com>
2020-01-06 00:27:12 -0300

    Histórico de execução com Rake

--- enjoeat/features/support/hooks.rb
+++ enjoeat/features/support/hooks.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 Before do
   # page.current_window.resize_to(0, 0)
   page.driver.browser.manage.window.maximize
@@ -14,4 +16,12 @@ end
 
 Before("@green_food") do
   visit "/restaurants/green-food/menu"
+end
+
+After do |scenario|
+  if scenario.failed?
+    shot_file = page.save_screenshot("log/screenshot.png")
+    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+    embed(shot_b64, "image/png", "Screenshot") # cucumber anexa screenshot no relatório
+  end
 end
