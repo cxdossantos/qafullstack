f3eb17fd29b477587d14bc764c12af0dfb71aa62
Cleiton Xavier <cxdossantos@gmail.com>
2020-01-21 09:50:29 -0300

    Adicionando browser no Rake

--- enjoeat/features/support/env.rb
+++ enjoeat/features/support/env.rb
@@ -2,8 +2,19 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+@browser = ENV["BROWSER"]
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
+  case @browser
+  when "chrome"
+    @driver = :selenium_chrome
+  when "chrome_headless"
+    @driver = :selenium_chrome_headless
+  when "firefox"
+    @driver = :selenium
+  end
+
+  config.default_driver = @driver
   config.app_host = "https://enjoeat-sp4.herokuapp.com"
   config.default_max_wait_time = 10
 end
